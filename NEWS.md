a123e9672ff150883877b9a09f3833fe04d80dfc
Martin R. Smith <martin.smith@durham.ac.uk>
2020-07-01 11:00:27 +0000

    version 1.0.0

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# TBRDist 1.0.0
+
+- Initial implementation of distances on unrooted trees.
---
+++ R/zzz.R
@@ -0,0 +1,21 @@
+#' @useDynLib TBRDist
+.onUnload <- function (libpath) {
+  library.dynam.unload("TBRDist", libpath)
+}
+
+## Reminders when releasing for CRAN
+release_questions <- function() {
+  c(
+    "Is the code free of #TODOs?",
+    "Have you checked the output of TBRDist.Rmd?",
+    "Have you updated the version number in NEWS.md & DESCRIPTION?"
+  )
+}
+
+# Additional tests:
+#
+# spell_check()
+# pkgdown::build_reference_index()
+# check_win_devel(); rhub::check_for_cran()
+# codemetar::write_codemeta()
+# # revdepcheck::revdep_check()
